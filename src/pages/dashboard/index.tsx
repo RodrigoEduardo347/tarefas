7b7cd5aa479f3750868f9ee88dd3cfd15192c0c3
Rodrigo Eduardo da Silva <rodrigoeduardo347@gmail.com>
2023-03-18 09:40:27 -0300

    Criando login e rota de admin

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,53 @@
+import TextArea from '@/components/textarea';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 import styles from './styles.module.css';
 
-export default function Dashboard(){
+export default function Dashboard() {
     return (
         <div className={styles.container}>
             <Head>
                 <title>Meu painel de tarefas</title>
             </Head>
 
-            <h1>Página Painel</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+                        <form>
+                            <TextArea placeholder="Digite qual a sua tarefa..." />
+                            <div className={styles.checkboxArea}>
+                                <input type="checkbox" className={styles.checkbox} />
+                                <label>Deixar tarefa pública?</label>
+
+                            </div>
+                            <button type="submit" className={styles.button}>
+                                Registrar
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+
+    const session = await getSession({ req });
+
+    if (!session?.user) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {},
+    }
 }
